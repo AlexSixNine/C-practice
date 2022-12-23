87455f736a6413569e7d774c6065aeae9531218d
AlexSixNine <116283933+AlexSixNine@users.noreply.github.com>
2022-12-23 16:17:42 +0100

    Create substring.h

---
+++ Substring/substring.h
@@ -0,0 +1,9 @@
+#ifndef SUBSTRING_H
+#define SUBSTRING_H
+
+#include <cstring>
+
+int const MAX = 100;
+
+int substring_position(char *string1, char *string2);
+bool is_prefix(char *string1, char *string2);
